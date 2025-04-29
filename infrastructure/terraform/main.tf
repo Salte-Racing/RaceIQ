0a95937a2861ecf9320b699294cd8c15f6d69f71
davewoodward <dave@salte.io>
2025-04-29 13:59:07 -0400

    Fix backend code location.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -130,7 +130,7 @@ resource "aws_lambda_function" "backend" {
 # Archive the Lambda function code
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../backend/dist"
+  source_dir  = "${path.module}/../../dist/backend"
   output_path = "${path.module}/lambda.zip"
 }
 
